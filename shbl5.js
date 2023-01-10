9d4f52c1574e54d4ebf41d1448fed1b2073e24ac
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-01-10 03:04:26 +0300

    Update shbl5.js

--- shbl5.js
+++ shbl5.js
@@ -196,5 +196,91 @@ var l_itvsig=setInterval(function(){
 
 
 
+$("style").last().append(`
+
+
+
+
+#users .uzr.shbl1 img.ustat {
+      width: 0px !important;
+      border-radius: 100px;
+      height: 0px;
+     
+      
+    }
+
+#users .uzr.shbl1 .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 9px !important;
+  height: 52px !important;
+  margin-left: 12px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+}
+
+#users .uzr.shbl1 .u-msg {
+	text-align: center;
+  -webkit-background-clip: text;
+  padding: 1px !important;
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2022/12/167226515511691.gif);
+  padding: 0px !important;
+  font-size: 89% !important;
+	margin-bottom: 6px !important;
+background-size: cover;
+margin-top: 5px !important;
+
+}
+
+
+
+
+
+#users .uzr.shbl1 {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 4px;
+  background-image: 
+  background-size: cover;
+  border: 1px solid #fff;
+background-image: linear-gradient(-225deg, rgb(255, 192, 0) 0%, rgba(255, 255, 0, 0.01) 8%, rgba(255, 255, 0, 0.01) 92%, rgb(255, 192, 0) 100%);
+    
+}
+
+
+
+#users .uzr.shbl1 .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.shbl1 .u-topic {
+  margin-left: 5px;
+}
+
+#users .uzr.shbl1 .My_star24 {
+ width: 72px;
+height: 72px;
+margin-top: -10px;
+cursor: pointer;
+margin-left: -10px;
+}
+
+
+
+
+`);
+
+var l_itvshbl=setInterval(function(){
+    if(myid!=null){
+        clearInterval(l_itvshbl);
+        $("#users .uzr:contains('*1+')").addClass('shbl1');
+        $("#users .uzr.shbl1 .fitimg.u-pic").append('<img class="My_star24" title="اطاري" src="https://up6.cc/2022/12/167209273598672.png">');
+     
+
+    }
+}, 20000);
+
 
 
