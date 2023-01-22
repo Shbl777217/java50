90016e86abe6de2eedfebc08f5bedf6600f2deb2
Shbl777217 <115428778+Shbl777217@users.noreply.github.com>
2023-01-22 16:33:17 +0300

    Update shbl5.js

--- shbl5.js
+++ shbl5.js
@@ -775,7 +775,7 @@ margin-left: -40px;
 var l_itvsid=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvsid);
-       $("#users .uzr:contains('ƒ')").addClass('sida1');
+       $("#users .uzr:contains('ד')").addClass('sida1');
 		
 		
 		
